c4d8099b39d4dab473695b118f3b24ecc9bdb79e
Tu Thanh Nguyen <tunguyen220197@gmail.com>
2024-08-21 22:42:30 +0300

    Play Audio Source
    added rocket thrust audio and codes for the audio to play when rocket thrusts and stops when it stops in Movement.cs. Also disable Play on Awake in AudioSource component

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -10,11 +10,13 @@ public class Movement : MonoBehaviour
     [SerializeField]
     float rotationThrust = 10f;
     Rigidbody rb;
+    AudioSource audioSource;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -29,6 +31,14 @@ public class Movement : MonoBehaviour
         if (Input.GetKey(KeyCode.Space))
         {
             rb.AddRelativeForce(Vector3.up * mainThrust * Time.deltaTime);
+            if (!audioSource.isPlaying)
+            {
+                audioSource.Play();
+            }
+        }
+        else
+        {
+            audioSource.Stop();
         }
     }
 
